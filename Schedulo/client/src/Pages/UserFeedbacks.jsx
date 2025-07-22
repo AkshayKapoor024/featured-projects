b23ce1d73d5c8e9e922cbe84afa37d415fda4595
AKSHAY KAPOOR <work.akshaykapoor24@gmail.com>
2025-07-23 00:41:48 +0530

    Update UserFeedbacks.jsx

--- Schedulo/client/src/Pages/UserFeedbacks.jsx
+++ Schedulo/client/src/Pages/UserFeedbacks.jsx
@@ -10,7 +10,9 @@ export default function UserFeedback(){
     useEffect(()=>{
         const checkAuth = async()=>{
 
-            const response =await axios.get('https://schedulo-server-pfcu.onrender.com/isAuthenticated')
+            const response =await axios.get('https://schedulo-server-pfcu.onrender.com/isAuthenticated',{
+  withCredentials: true,
+})
             if(!response.data.email){
                 toast.error('Error:Signup to Acess this feature!')
                 navigate('/signup')
