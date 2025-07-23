873fa1f8dc2080a05b778d00b1634e1d54b87fe4
AKSHAY KAPOOR <work.akshaykapoor24@gmail.com>
2025-07-24 00:20:21 +0530

    Update EmptyList.jsx

--- Schedulo/client/src/components/DetailedCard/EmptyList.jsx
+++ Schedulo/client/src/components/DetailedCard/EmptyList.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function EmptyList({path}) {
     return (
         <div className="2xl:h-[600px] flex flex-col gap-5 relative overflow-hidden mt-2">
-            <h1 className="text-3xl flex justify-center items-center font-semibold text-gray-600 z-10">
+            <h1 className="text-3xl flex justify-center text-center md:text-left items-center font-semibold text-gray-600 z-10">
                 Oops !! Your event has no registered users.
             </h1>
             <Link to={path} className=" flex justify-center items-center text-blue-600 underline text-lg">Go back to event page?</Link>
