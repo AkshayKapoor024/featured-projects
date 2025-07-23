2c78053e66cbea284daff256f90151e7195c1915
AKSHAY KAPOOR <work.akshaykapoor24@gmail.com>
2025-07-24 00:17:05 +0530

    Update MyRsvp.jsx

--- Schedulo/client/src/components/Dashboard/MyRsvp.jsx
+++ Schedulo/client/src/components/Dashboard/MyRsvp.jsx
@@ -28,16 +28,16 @@ export default function MyRsvpList({ title }) {
   });
 
   return (
-    <div
-      className="flex justify-center items-center text-gray-900 bg-gray-100"
+   <div
+      className="flex justify-center items-center h-full w-[90vw] sm:w-[90vw] md:w-[90vw] lg:w-[80vw] xl:w-[70vw] 2xl:w-[56vw] text-gray-900 bg-gray-100"
       style={{ fontFamily: "Montserrat,sans-serif" }}
     >
-      <div className="flex flex-col bg-white 2xl:w-[950px] 2xl:min-h-[800px] shadow-xl rounded-2xl">
+      <div className="flex flex-col w-full h-full shadow-xl rounded-2xl">
         <h1 className="h-20 p-5 text-4xl font-bold shadow-sm bg-indigo-600 text-white rounded-r-2xl">
           My RSVPs for {title}
         </h1>
 
-        <div className="flex-1 max-h-[700px] overflow-y-auto w-full overflow-x-hidden">
+        <div className="flex-1 overflow-y-auto w-full overflow-x-hidden p-2">
           {list.length === 0 ? (
             <EmptyList path={path} />
           ) : (
