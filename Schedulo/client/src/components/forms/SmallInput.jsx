0c3cf93d07c2bad0b83538c644218e7855b56d86
AKSHAY KAPOOR <work.akshaykapoor24@gmail.com>
2025-07-24 00:19:55 +0530

    Update SmallInput.jsx

--- Schedulo/client/src/components/forms/SmallInput.jsx
+++ Schedulo/client/src/components/forms/SmallInput.jsx
@@ -2,15 +2,15 @@ export default function SmallInput({ type = 'text', label, placeholder , value ,
   const pattern = type === 'email' ? '[^@\\s]+@[^@\\s]+\\.[^@\\s]+' : undefined;
 
   return (
-    <div className="grid grid-cols-7 items-center gap-2 h-24">
-      <label className="col-span-3 2xl:col-span-3 text-center text-xl font-semibold text-gray-600">{label}</label>
-      <div className="flex mx-2 items-center col-span-5 2xl:mx-0 2xl:col-span-3 ">
+   <div className="grid grid-cols-7 items-center gap-2 h-24">
+      <label className="col-span-7 2xl:col-span-3 text-center text-xl font-semibold text-gray-600">{label}</label>
+      <div className="flex mx-2 items-center  col-span-5 2xl:mx-0 2xl:col-span-3 ">
         <input
           type={type}
           required
           pattern={pattern}
           placeholder={placeholder}
-          className="input input-info focus:ring-1 focus:ring-indigo-200 bg-white w-[150px] sm:w-[600px] 2xl:w-[700px] validator  text-gray-600"
+          className="input input-info focus:ring-1 focus:ring-indigo-200 bg-white w-[100vw] sm:w-[600px] 2xl:w-[700px] validator  text-gray-600"
           value={value}
           onChange={onChange}
           name={name}
